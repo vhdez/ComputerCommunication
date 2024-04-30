6a9c491c361d2933c41c9ad0016f06450e4acf3b
vhdez <vhernandez@scienceleadership.org>
2024-04-30 11:47:57 -0400

    Server can receive many messages from Client

--- src/Client.java
+++ src/Client.java
@@ -6,11 +6,14 @@ public class Client {
     public static void main(String[] args) throws Exception {
         System.out.println("Connecting to my server");
         Socket newSocket = new Socket("127.0.0.1",3256);
+
         OutputStream out = newSocket.getOutputStream();
         ObjectOutputStream objOut = new ObjectOutputStream(out);
+
         objOut.writeObject("FIRST from Mr. H");
         objOut.writeObject("SECOND from Mr. H");
         objOut.writeObject("THIRD from Mr. H");
+
         System.out.println("Done sending messages");
     }
 }
